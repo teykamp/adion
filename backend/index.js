3dac0052d4df37d63050e59f1a8b611c6444dace
abhijit <kamathabhijit2002@gmail.com>
2024-03-24 21:57:18 -0400

    set up db tables and began experimenting with the supabase client

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,28 @@
-require("dotenv").config();
-const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
+import 'dotenv/config';
+import express  from 'express';
+import  cors from "cors"
+import { createClient } from '@supabase/supabase-js';
 
 const app = express();
+
+app.use(cors);
+
 const SB = createClient(process.env.API_URL, process.env.API_KEY);
+
+// app.put("/riders/create", (re) =>)
+app.get("/", (req,res) =>{
+    res.status(200).json({message: "testing"})
+})
+app.get("/users", async (req,res) =>{
+    const {data, error} = await SB.from("User").select("*").limit(5);
+    console.log(data);
+    console.log(error);
+    res.status(200).json({message: "testing"});
+})
+app.get("/users/:uuid", (req,res) =>{})
+app.put("/users/create", (req,res) =>{})
+app.delete("/users/uuid", (req,res) =>{})
+
+app.listen(3000, () => {
+    console.log(`server running on ${3000}`);
+})

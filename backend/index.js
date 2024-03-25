d4b860cc9f7e5132e7e9dee3a557964ff2133429
abhijit <kamathabhijit2002@gmail.com>
2024-03-24 23:02:43 -0400

    began with api stuff

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,35 @@
-import 'dotenv/config';
-import express  from 'express';
-import  cors from "cors"
-import { createClient } from '@supabase/supabase-js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import "crypto";
+import { createClient } from "@supabase/supabase-js";
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
 
 const SB = createClient(process.env.API_URL, process.env.API_KEY);
 
 // app.put("/riders/create", (re) =>)
-app.get("/", (req,res) =>{
-    res.status(200).json({message: "testing"})
-})
-app.get("/users", async (req,res) =>{
-    const {data, error} = await SB.from("User").select("*").limit(5);
-    console.log(data);
-    console.log(error);
-    res.status(200).json({message: "testing"});
-})
-app.get("/users/:uuid", (req,res) =>{})
-app.put("/users/create", (req,res) =>{})
-app.delete("/users/uuid", (req,res) =>{})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "testing" });
+});
+app.get("/users", async (req, res) => {
+  const { data, error } = await SB.from("User").select("*").limit(5);
+  console.log(data);
+  console.log(error);
+  res.status(200).json({ data: data });
+});
+app.get("/users/:uuid", (req, res) => {});
+app.post("/users/create", async (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ data: "testing" });
+  //   new_user = req.body();
+  //   new_user.id = crypto.randomUUID();
+  //   const { error } = await SB.from("User").insert();
+});
+app.delete("/users/uuid", (req, res) => {});
 
 app.listen(3000, () => {
-    console.log(`server running on ${3000}`);
-})
+  console.log(`server running on ${3000}`);
+});
